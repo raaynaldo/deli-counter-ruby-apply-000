c612b618dcd4dc5d3e9a6810b27813fc53843abc
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-23 01:09:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 def now_serving(arr)
   if( arr.length !=0)
     puts "Currently serving #{arr[0]}."
-    arr.unshift
+    arr.unshift()
   else
     puts "There is nobody waiting to be served!"
   end
