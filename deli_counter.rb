22ce9cafbfb1e20f297a2b35d0c41e8693c77e75
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-23 00:40:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,7 @@ katz_deli = []
 def line(arr)
   if arr.length != 0
     message = "The line is currently:"
-    arr.each_index do |i|
-      message += " #{i+1}. #{arr[i]}"
-    end
+    arr.each_index {|i| message += " #{i+1}. #{arr[i]}"}
     puts message
   else
     puts "The line is currently empty."
